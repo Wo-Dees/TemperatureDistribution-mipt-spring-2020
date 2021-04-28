ca93e825d9840e8746c0ec450ecd0d742a7c696a
Karim Vafin <karim.vafin@mail.ru>
2021-04-28 19:12:26 +0300

    connected SFML

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-#include "special_number.h"
+#include <SFML/Graphics.hpp>
 
-int main() {
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
 
-    return 0;
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
-
